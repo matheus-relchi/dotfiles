d44349c30738481691d583b4bc893f88878dc9ab
Matheus Relchi <matheusrelchicontato@outlook.com.br>
2023-03-28 14:02:30 -0300

    First commit

---
+++ README.md
@@ -0,0 +1,5 @@
+## Made for Arch-Linux with all my bloat
+
+1. ```cd dotfiles/linux```
+
+2. ```./install.sh```
---
+++ install.sh
@@ -0,0 +1,40 @@
+#!/bin/sh
+
+if [ "$UID" -eq 0 ]; then
+    echo 'This script should not be run as root' >&2
+    exit 1
+fi
+
+echo "Getting sudo ready"
+sudo -v
+
+echo "Moving dotfiles to home"
+sudo pacman -S --noconfirm rsync 
+rsync -av go_to_home/ ~
+echo "Moved to home"
+
+echo "Installing packages"
+sudo pacman -Sy
+sudo pacman -S --needed --noconfirm $(cat installList)
+echo "Packages installed"
+
+echo "Installing yay Aur Helper"
+rm -rf yay > /dev/null
+git clone https://aur.archlinux.org/yay.git
+cd yay
+makepkg -si
+rm -rf yay > /dev/null
+cd ~
+echo "Yay installed"
+
+echo "Installing zsh plugins"
+mkdir -p ~/.zsh/plugins
+git clone https://github.com/zsh-users/zsh-autosuggestions ~/.zsh/plugins/zsh-autosuggestions/
+git clone https://github.com/zdharma-continuum/fast-syntax-highlighting ~/.zsh/plugins/fast-syntax-highlighting/
+echo "zsh plugins installed"
+
+echo "Changing zsh to default shell"
+chsh -s /usr/bin/zsh
+echo "ZSH is the default shell now"
+
+echo "Setup complete"
